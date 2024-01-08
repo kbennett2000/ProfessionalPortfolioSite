3cb0e75cddc885cd0d8c413dd0b16701322fe3f4
Kris Bennett <kbennett2000@hotmail.com>
2024-01-07 18:31:23 -0700

    Add files via upload

---
+++ frontend_react/src/config.js
@@ -0,0 +1,24 @@
+// config.js
+const config = {
+    greetingMessage: "Hello, I am ",
+    greetingEmoji: "👋",
+    ownerName: "Kris Bennett",
+    skill01: "Technical Education",
+    skill02: "Custom Solution Development",
+    skill03: "Enterprise Moderinzation",
+    tagLine: "I Know that <span>Great Execution</span> <br />equals <span>Great Results",
+    workPageTitle: "My Professional <span>Portfolio</span> Section",
+    portfolioSections: [ 'Enterprise Modernization', 'Custom Development', 'Web3', 'Education', 'All' ],
+    skillsPageTitle: "Skills & Experiences",
+    footerTitle: "Let's chat!",
+    contactEmail: "kbennett2000@gmail.com",
+    contactPhone: "+1 (303) 535-5361",
+    copyrightText01: "@2024-2025 Kris Bennett",
+    copyrightText02: "All rights reserved",
+    sentMessageThankYou: "Thank you for sending me a message, I'll be in touch shortly!",
+    LinkedInURL: "https://www.linkedin.com/in/kbennett2000/",
+    GitHubURL: "https://github.com/kbennett2000",
+  };
+  
+  export default config;
+  
